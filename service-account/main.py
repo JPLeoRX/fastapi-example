94b712956939c507cbfda7dc26161464ac034946
leo <leo.ertuna@gmail.com>
2021-01-07 01:39:59 +0300

    Use injected utils object

--- service-account/main.py
+++ service-account/main.py
@@ -1,10 +1,11 @@
 from injectable import load_injection_container
+load_injection_container()
+
 from fastapi import FastAPI
 from resource_ping import router_ping
 from resource_account import router_account
 
 
-load_injection_container()
 app = FastAPI()
 app.include_router(router_ping)
 app.include_router(router_account)
--- service-account/service_account.py
+++ service-account/service_account.py
@@ -1,8 +1,14 @@
-from injectable import injectable
+from injectable import injectable, autowired, Autowired
 from account_model import AccountModel
+from utils_id import UtilsId
 
 
 @injectable
 class ServiceAccount:
+    @autowired
+    def __init__(self, utils_id: Autowired(UtilsId)):
+        self.utils_id = utils_id
+
     def get(self, id: str) -> AccountModel:
+        print(self.utils_id.generate_uuid())
         return AccountModel(id, 'my-username', 'my-password', 'my-image', -1)
